4b080676c96795a8db046de1378f15a9a87e00ff
PassionME <37819919+Passionme@users.noreply.github.com>
2018-05-31 09:22:37 -0400

    Add files via upload

--- ScanXMLtoDataFrame.py
+++ ScanXMLtoDataFrame.py
@@ -1,35 +1,51 @@
-# import xml.etree.ElementTree as ET
 import pandas as pd
 import io
 import lxml.etree as ET
 from lxml import etree
 import xlsxwriter
 
-df = pd.DataFrame(columns=["ContectName","RegisteredName","ValidVotes"])
-
+COL = ["RegisteredName","ValidVotes"]
+df2 = pd.DataFrame(columns = COL )
+df = pd.DataFrame()
 tree = ET.parse('Telling_TK2017_gemeente_Aalsmeereml.xml')
 root = tree.getroot()
-content = etree.tostring(root)
-contentBuffer=io.BytesIO(content)
-context = ET.iterparse(contentBuffer,tag='Contest')
-for child in root.iter():
-    print(tree.getpath(child))
-for action, elem in context:
-    print(elem.xpath('/Contest/ContestIdentifier/ContestName/text()'))
-    id=elem.xpath('id')
-    # RegName=elem.xpath('Selection/AffiliationIdentifier/RegisteredName/')
-    # for field,value in zip(fields,values):
-    #     print('\t{f} = {v}'.format(f=field,v=value))
+# content = etree.tostring(root)
+# contentBuffer=io.BytesIO(content)
+# context = ET.iterparse(contentBuffer,tag='Contest')
+# print(tree, tree.find('Count'))
+l3=[]
+l4=[]
+for child1 in root.iter():
+    if child1.getchildren():
+        for child2 in child1.iter():
+            if child2.getchildren():
+                for child3 in child2.iter():
+                    if child3.getchildren():
+                        for child4 in child3.iter():
+                            if child4.text :
+                                if len(l4) == 0 and not (child4.text).isalpha():
+                                    pass
+                                else:
+                                    l4.append(child4.text)
+                                    if len(l4) == 2:
+                                        # print(df.append(pd.DataFrame(l4)))
+                                        df = df.append(pd.DataFrame([l4],columns = COL), ignore_index = True)
+                                        # print(df)
+                                        l4 = []
+                                # print("child4",child4.tag, child4.text)
 
-
-# for child in root:
-#     print("yes", child.tag,child.getchildren())
-#     if ("Count" in child.tag) and  child.getchildren():
-#         for subchild1 in child.iter():
-#             for subchild2 in subchild1.iter():
-#                 for subchild3 in subchild2.iter():
-#                     df.append([subchild1,subchild2,subchild3])
-#                     # print("child", child.tag,"subchild", subchild3.tag)
+                    else:
+                        if child3.text:
+                            if len(l3) == 0 and not (child3.text).isalpha():
+                                pass
+                            else:
+                                l3.append(child3.text)
+                                if len(l3) == 2:
+                                    # print(pd.DataFrame(l3))
+                                    df = df.append(pd.DataFrame([l3],columns = COL) ,ignore_index = True)
+                                    # print(df)
+                                    l3 = []
+                        # print(df)
 
 # print(df)
 writer = pd.ExcelWriter('pandas_simple.xlsx', engine='xlsxwriter')
@@ -39,7 +55,3 @@ df.to_excel(writer, sheet_name='Sheet1')
 
 # Close the Pandas Excel writer and output the Excel file.
 writer.save()
-# parser = ET.XMLPullParser(['start','end'])
-# for event, elem in parser.read_events():
-#     print("event",event)
-#     print(elem.tag)
